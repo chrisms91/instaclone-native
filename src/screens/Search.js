998b4a3166e2bbc1b4d3a130d500c45cd8a19b91
Christopher Kim <chrisms91@gmail.com>
2021-04-23 16:06:51 -0700

    add loading indicator to search screen

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,21 +1,60 @@
 import React, { useEffect } from 'react';
+import { gql, useLazyQuery } from '@apollo/client';
 import { useForm } from 'react-hook-form';
-import { Text, TouchableOpacity, View, TextInput } from 'react-native';
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  ActivityIndicator,
+} from 'react-native';
 import styled from 'styled-components/native';
 import { colors } from '../colors';
 import DismissKeyboard from '../components/DismissKeyboard';
+import LoadingIndicator from '../components/LoadingIndicator';
+
+const SEARCH_PHOTOS = gql`
+  query searchPhotos($keyword: String!) {
+    searchPhotos(keyword: $keyword) {
+      id
+      file
+    }
+  }
+`;
 
 const Container = styled.View`
   flex: 1;
+  background-color: black;
+`;
+
+const MessageContainer = styled.View`
+  flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: black;
+`;
+
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
 `;
 
 const Input = styled.TextInput``;
 
 const Search = ({ navigation }) => {
-  const { setValue, register, watch } = useForm();
+  const { setValue, register, watch, handleSubmit } = useForm();
+  const [startQueryFn, { loading, data, called }] = useLazyQuery(SEARCH_PHOTOS);
+
+  const onValid = ({ keyword }) => {
+    startQueryFn({
+      variables: {
+        keyword,
+      },
+    });
+  };
+
+  console.log(data);
+
   const SearchBox = () => (
     <TextInput
       style={{ backgroundColor: 'white' }}
@@ -26,6 +65,7 @@ const Search = ({ navigation }) => {
       returnKeyType="search"
       returnKeyLabel="Search"
       onChangeText={(text) => setValue('keyword', text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
 
@@ -33,13 +73,30 @@ const Search = ({ navigation }) => {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register('keyword');
+    register('keyword', {
+      required: true,
+    });
   }, []);
 
   return (
     <DismissKeyboard>
       <Container>
-        <Text style={{ color: 'white' }}>Search</Text>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size="large" color="white" />
+            <MessageText>Searching...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>Search photos by keyword</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined && data?.searchPhotos.length === 0 ? (
+          <MessageContainer>
+            <MessageText>Could not find anything...</MessageText>
+          </MessageContainer>
+        ) : null}
       </Container>
     </DismissKeyboard>
   );
