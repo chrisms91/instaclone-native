b1c327bb31d611a02352fc6bcef3f019ee2b2315
Christopher Kim <chrisms91@gmail.com>
2021-05-05 01:51:53 -0700

    clean up rooms screen

---
+++ src/components/rooms/RoomItem.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { colors } from '../../colors';
+import useMe from '../../hooks/useMe';
+
+const RoomContainer = styled.TouchableOpacity`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 10px;
+`;
+
+const Column = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin-right: 20px;
+`;
+
+const Data = styled.View``;
+
+const UnreadIndicator = styled.View`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background-color: ${colors.blue};
+  margin-right: 5px;
+`;
+
+const Username = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const UnreadText = styled.Text`
+  color: white;
+  margin-top: 2px;
+  font-weight: 500;
+`;
+
+const RoomItem = ({ users, unreadTotal, id }) => {
+  const { data: meData } = useMe();
+  const notMe = users.find((user) => user.userName !== meData?.me?.userName);
+
+  return (
+    <RoomContainer>
+      <Column>
+        <Avatar source={{ uri: notMe.avatar }} />
+        <Data>
+          <Username>{notMe.userName}</Username>
+          <UnreadText>
+            {unreadTotal} unread {unreadTotal === 1 ? 'message' : 'messages'}
+          </UnreadText>
+        </Data>
+      </Column>
+      <Column>{unreadTotal > 0 ? <UnreadIndicator /> : null}</Column>
+    </RoomContainer>
+  );
+};
+
+export default RoomItem;
--- src/screens/Rooms.js
+++ src/screens/Rooms.js
@@ -7,6 +7,7 @@ import ScreenLayout from '../components/ScreenLayout';
 import useMe from '../hooks/useMe';
 import { colors } from '../colors';
 import ItemSeparator from '../components/ItemSeparator';
+import RoomItem from '../components/rooms/RoomItem';
 
 const SEE_ROOMS_QUERY = gql`
   query seeRooms {
@@ -17,73 +18,10 @@ const SEE_ROOMS_QUERY = gql`
   ${ROOM_FRAGMENT}
 `;
 
-const RoomContainer = styled.TouchableOpacity`
-  width: 100%;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 15px 10px;
-`;
-
-const Column = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const Avatar = styled.Image`
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
-  margin-right: 20px;
-`;
-
-const Data = styled.View``;
-
-const UnreadIndicator = styled.View`
-  width: 10px;
-  height: 10px;
-  border-radius: 5px;
-  background-color: ${colors.blue};
-  margin-right: 5px;
-`;
-
-const Username = styled.Text`
-  color: white;
-  font-weight: 600;
-  font-size: 16px;
-`;
-
-const UnreadText = styled.Text`
-  color: white;
-  margin-top: 2px;
-  font-weight: 500;
-`;
-
 const Rooms = () => {
   const { data, loading } = useQuery(SEE_ROOMS_QUERY);
   const { data: meData } = useMe();
-  const renderItem = ({ item: room }) => {
-    const notMe = room.users.find(
-      (user) => user.userName !== meData?.me?.userName
-    );
-
-    return (
-      <RoomContainer>
-        <Column>
-          <Avatar source={{ uri: notMe.avatar }} />
-          <Data>
-            <Username>{notMe.userName}</Username>
-            <UnreadText>
-              {room.unreadTotal} unread{' '}
-              {room.unreadTotal === 1 ? 'message' : 'messages'}
-            </UnreadText>
-          </Data>
-        </Column>
-        <Column>{room.unreadTotal > 0 ? <UnreadIndicator /> : null}</Column>
-      </RoomContainer>
-    );
-  };
+  const renderItem = ({ item: room }) => <RoomItem {...room} />;
   return (
     <ScreenLayout loading={loading}>
       <FlatList
