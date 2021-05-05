9a9b0e22a566790bfba162e4fba6cca885b0261f
Christopher Kim <chrisms91@gmail.com>
2021-05-05 01:47:36 -0700

    styling rooms screen

--- src/navigators/MessagesNav.js
+++ src/navigators/MessagesNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
 import Room from '../screens/Room';
 import Rooms from '../screens/Rooms';
 
@@ -7,7 +8,23 @@ const Stack = createStackNavigator();
 
 const MessagesNav = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: 'white',
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: 'black',
+        },
+        headerBackImage: ({ tintColor }) => (
+          <Ionicons
+            name="close"
+            color={tintColor}
+            size={28}
+            style={{ marginLeft: 5 }}
+          />
+        ),
+      }}
+    >
       <Stack.Screen name="Rooms" component={Rooms} />
       <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
--- src/screens/Rooms.js
+++ src/screens/Rooms.js
@@ -1,10 +1,100 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components/native';
+import { ROOM_FRAGMENT } from '../fragments';
+import ScreenLayout from '../components/ScreenLayout';
+import useMe from '../hooks/useMe';
+import { colors } from '../colors';
+import ItemSeparator from '../components/ItemSeparator';
 
-const Rooms = () => (
-  <View>
-    <Text>Room List</Text>
-  </View>
-);
+const SEE_ROOMS_QUERY = gql`
+  query seeRooms {
+    seeRooms {
+      ...RoomParts
+    }
+  }
+  ${ROOM_FRAGMENT}
+`;
+
+const RoomContainer = styled.TouchableOpacity`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 10px;
+`;
+
+const Column = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin-right: 20px;
+`;
+
+const Data = styled.View``;
+
+const UnreadIndicator = styled.View`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background-color: ${colors.blue};
+  margin-right: 5px;
+`;
+
+const Username = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const UnreadText = styled.Text`
+  color: white;
+  margin-top: 2px;
+  font-weight: 500;
+`;
+
+const Rooms = () => {
+  const { data, loading } = useQuery(SEE_ROOMS_QUERY);
+  const { data: meData } = useMe();
+  const renderItem = ({ item: room }) => {
+    const notMe = room.users.find(
+      (user) => user.userName !== meData?.me?.userName
+    );
+
+    return (
+      <RoomContainer>
+        <Column>
+          <Avatar source={{ uri: notMe.avatar }} />
+          <Data>
+            <Username>{notMe.userName}</Username>
+            <UnreadText>
+              {room.unreadTotal} unread{' '}
+              {room.unreadTotal === 1 ? 'message' : 'messages'}
+            </UnreadText>
+          </Data>
+        </Column>
+        <Column>{room.unreadTotal > 0 ? <UnreadIndicator /> : null}</Column>
+      </RoomContainer>
+    );
+  };
+  return (
+    <ScreenLayout loading={loading}>
+      <FlatList
+        data={data?.seeRooms}
+        keyExtractor={(room) => '' + room.id}
+        renderItem={renderItem}
+        style={{ width: '100%' }}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+      />
+    </ScreenLayout>
+  );
+};
 
 export default Rooms;
